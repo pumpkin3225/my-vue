cd7d7e1c2aa12fa2024f5610bf227575018a265e
pumpkin3225 <152354723+pumpkin3225@users.noreply.github.com>
2024-01-11 15:50:04 +0800

    create search function

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -35,7 +35,10 @@ export default {
                 }
             ],
             weatherData: [],
-            selectedCounty: '台灣各縣市',
+            selectedCounty: {
+                title: '台灣各縣市',
+                input: '',
+            },
             url: 'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWA-772C064A-6F46-498D-BC1C-FDC4CF8B67EA',
             locationSort: ['臺北市', '新北市', '基隆市', '新竹市', '桃園市', '新竹縣', '宜蘭縣', '臺中市', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '高雄市', '臺南市', '嘉義市', '嘉義縣', '屏東縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣'],
         };
@@ -51,24 +54,24 @@ export default {
                 });
             })
     },
-
     computed: {
-        getCounty() {
-            if (this.selectedCounty === '台灣各縣市') {
-                return this.weatherData
-            }
-            else {
-                const selectedCounties = this.selectedCounty.split(',');
+        filteredCounty() {
+            if (this.selectedCounty.title === '台灣各縣市') {
+                return this.weatherData.filter(item => item.locationName.includes(this.selectedCounty.input));
+            } else {
+                const selectedCounties = this.selectedCounty.title.split(',');
                 return this.weatherData.filter(item => {
-                    return selectedCounties.some(county => item.locationName.includes(county.trim()));
+                    selectedCounties.includes(item.locationName)
+                    return selectedCounties.includes(item.locationName) && item.locationName.includes(this.selectedCounty.input);
                 });
             }
         },
     },
 
     methods: {
-        updateSelectedCounty(selectedCounty) {
-            this.selectedCounty = selectedCounty.join(',');
+        updateSelectedCounty(countyArea) {
+            this.selectedCounty.title = countyArea.join(',');
+            this.selectedCounty.input = ''
         },
     },
 }
@@ -82,17 +85,18 @@ export default {
             </WeatherBtn>
         </div>
         <div>
-            <select v-model="selectedCounty" class="w-full cursor-pointer " aria-label="Default select example">
-                <option v-for="county in counties" :key="county.id" :value="county.area.join()">{{ county.title }}
-                </option>
+            <select v-model="selectedCounty.title" class="w-[50%] cursor-pointer " aria-label="Default select example">
+                <option v-for="county in counties" :key="county.id" :value="county.area.join()">{{ county.title }}</option>
             </select>
+            搜尋: <input type="text" v-model.trim="selectedCounty.input">
             <ul class=" bg-[#8DD7CF] border-[1px] p-2 text-center">
                 <li class="">
-                    {{ selectedCounty }}
+                    {{ selectedCounty.title }}
                 </li>
             </ul>
             <div class="flex flex-wrap  overflow-y-scroll h-[500px]">
-                <div v-for="weather in getCounty" :key="weather.id" class=" w-[50%] bg-[#E9A2AD] border-[1px] p-3 flex ">
+                <div v-for="weather in filteredCounty" :key="weather.id" @keyup.enter="clearInput"
+                    class=" w-[50%] bg-[#E9A2AD] border-[1px] p-3 flex ">
                     <WeatherCard :weather="weather" />
                 </div>
             </div>
